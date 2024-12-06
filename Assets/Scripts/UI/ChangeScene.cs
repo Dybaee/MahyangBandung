a704b99ae8e3fc9fb25e2a19726513f605b2649f
Dybaee <daniartha.my@gmail.com>
2024-12-06 19:00:54 +0700

    Small Changes

---
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public Animator anim;
+
+    public void NewStartGameOnClicked()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    IEnumerator StartGame()
+    {
+        anim.SetTrigger("Press");
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadSceneAsync("Gameplay");
+    }
+}
