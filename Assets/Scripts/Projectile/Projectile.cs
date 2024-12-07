ecc66de997ee4360bc25caa875a1ba0682f2adc5
AvKov <37872714+dupitydumb@users.noreply.github.com>
2024-12-07 13:40:47 +0700

    Projectile Fix

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Projectile : MonoBehaviour
 {
@@ -17,11 +18,16 @@ public class Projectile : MonoBehaviour
         set => _target = value;
     }
 
+    private VisualEffect _vfx;
     public TargetType targetType;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _vfx = GetComponentInChildren<VisualEffect>();
+        if (_vfx != null)
+        {
+            _vfx.Play();
+        }
     }
 
     // Update is called once per frame
@@ -34,10 +40,12 @@ public class Projectile : MonoBehaviour
             if (_target.position.x < transform.position.x)
             {
                 transform.Translate(Vector3.left * _speed * Time.deltaTime);
+                transform.localScale = new Vector3(-1, 1, 1);
             }
             else
             {
                 transform.Translate(Vector3.right * _speed * Time.deltaTime);
+                transform.localScale = new Vector3(1, 1, 1);
             }
         }
         else
