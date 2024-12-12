3ff00fde1e3f09ba72ef97febb4b8eb79c54fa9c
AvKov <37872714+dupitydumb@users.noreply.github.com>
2024-12-12 13:26:38 +0700

    Fix Chase Enemy, Enemy Animation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -30,6 +30,8 @@ public class Enemy : MonoBehaviour
         get => _damage;
         set => _damage = value;
     }
+    [SerializeField]
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -45,8 +47,8 @@ public class Enemy : MonoBehaviour
 
     void Move()
     {
-        GameObject player = getNearestPlayer();
-        if (player != null && Vector3.Distance(player.transform.position, transform.position) < _chaseRange)
+
+        if (player != null)
         {
             float distance = Vector3.Distance(player.transform.position, transform.position);
             Vector3 direction = player.transform.position - transform.position;
@@ -59,8 +61,9 @@ public class Enemy : MonoBehaviour
                 transform.localScale = new Vector3(1.37f, 1.37f, 1.37f);
             }
 
-            if (distance > _attackRange)
+            if (distance < _chaseRange)
             {
+                Debug.Log("Chasing player");
                 transform.position = Vector3.MoveTowards(transform.position, player.transform.position, _moveSpeed * Time.deltaTime);
                 if (_animator.GetCurrentAnimatorClipInfo(0)[0].clip.name != "IsWalking")
                 {
@@ -71,39 +74,20 @@ public class Enemy : MonoBehaviour
                 _animator.SetBool("IsWalking", true);
                 //check direction
             }
-            else if (distance <= _attackRange)
+            else
             {
                 _animator.SetBool("IsWalking", false);
             }
         }
     }
 
-    GameObject getNearestPlayer()
-    {
-        Debug.Log("Finding player");
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        GameObject nearestPlayer = null;
-        float minDistance = Mathf.Infinity;
-        foreach (GameObject player in players)
-        {
-            float distance = Vector3.Distance(player.transform.position, transform.position);
-            if (distance < minDistance)
-            {
-                minDistance = distance;
-                nearestPlayer = player;
-            }
-        }
-        return nearestPlayer;
-    }
 
     IEnumerator Attack()
     {
         while (true)
         {
-            GameObject player = getNearestPlayer();
             if (player != null)
             {
-                Debug.Log("Player found");
                 float distance = Vector3.Distance(player.transform.position, transform.position);
                 if (distance < _attackRange)
                 {
@@ -119,7 +103,6 @@ public class Enemy : MonoBehaviour
 
     public void DealDamage()
     {
-        GameObject player = getNearestPlayer();
         if (player != null)
         {
             player.GetComponent<Player>().TakeDamage(_damage);
